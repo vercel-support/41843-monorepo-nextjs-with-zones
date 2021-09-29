70df8eb3c6ab1ecc49dd77a6ae1b50989aa9133d
William Li <email@williamli.dev>
2021-09-29 19:50:08 +0800

    points root /blog to another project

--- root/next.config.js
+++ root/next.config.js
@@ -7,11 +7,11 @@ module.exports = {
       },
       {
         source: '/blog',
-        destination: `http://41843-monorepo-nextjs-with-zones-blog.vercel-support.app/blog`,
+        destination: `https://40049-nextjs-with-zones-blog.vercel-support.app/blog`,
       },
       {
         source: '/blog/:path*',
-        destination: `http://41843-monorepo-nextjs-with-zones-blog.vercel-support.app/blog/:path*`,
+        destination: `https://40049-nextjs-with-zones-blog.vercel-support.app/blog/:path*`,
       }
     ]
   },
